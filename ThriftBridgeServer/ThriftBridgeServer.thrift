27d94618c4af4963367e673b824f0dc90d515081
Bryan Everly <bryan.everly@teradata.com>
2013-08-12 20:58:00 -0400

    Added ThriftBridgeServer

---
+++ ThriftBridgeServer/ThriftBridgeServer.thrift
@@ -0,0 +1,4 @@
+service LegacyClassService
+{
+  i32 AddTwoNumbers(1: i32 number1, 2: i32 number2)
+}
---
+++ ThriftBridgeServer/ThriftBridgeServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+using Thrift.Server;
+using Thrift.Transport;
+using LegacyDLL;
+
+namespace ThriftBridgeServer
+{
+    class LegacyClassHandler : LegacyClassService.Iface
+    {
+        LegacyClass legacy_class = new LegacyClass();
+
+        public int AddTwoNumbers(int number1, int number2)
+        {
+            return legacy_class.AddTwoNumbers(number1, number2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LegacyClassHandler handler = new LegacyClassHandler();
+                LegacyClassService.Processor processor = new LegacyClassService.Processor(handler);
+                TServerTransport serverTransport = new TServerSocket(9090);
+
+                TServer server = new TSimpleServer(processor, serverTransport);
+                Console.Write("Starting server...");
+                server.Serve();
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.StackTrace);
+            }
+        }
+    }
+}
