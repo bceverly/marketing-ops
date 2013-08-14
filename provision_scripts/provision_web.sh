0ad9819f321a7741ff26e3c0998a189b81790346
Bryan Everly <bryan.everly@teradata.com>
2013-08-14 09:30:57 -0400

    Modified provisioning script to get Apache up on the base box

--- provision_scripts/provision_web.sh
+++ provision_scripts/provision_web.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
 LOCKFILE=/tmp/provision_web
+DEFAULT_SITE=/etc/apache2/sites-available/default
+
 if [ -e ${LOCKFILE} ];
 then
   exit 0
 fi
 
 echo '192.168.56.101 db db.local' >> /etc/hosts
-sudo forever start -a /srv/api/test.js
+#sudo forever start -a /srv/api/test.js
+
+# Set up the default apache2 site
+sudo echo '<VirtualHost *:80>' > ${DEFAULT_SITE}
+sudo echo '  DocumentRoot /srv/website' >> ${DEFAULT_SITE}
+sudo echo '  <Directory />' >> ${DEFAULT_SITE}
+sudo echo '    Options FollowSymLinks' >> ${DEFAULT_SITE}
+sudo echo '    AllowOverride None' >> ${DEFAULT_SITE}
+sudo echo '  </Directory>' >> ${DEFAULT_SITE}
+sudo echo '  <Directory /srv/website/>' >> ${DEFAULT_SITE}
+sudo echo '    Options Indexes FollowSymLinks MultiViews' >> ${DEFAULT_SITE}
+sudo echo '    AllowOverride None' >> ${DEFAULT_SITE}
+sudo echo '    Order allow,deny' >> ${DEFAULT_SITE}
+sudo echo '    allow from all' >> ${DEFAULT_SITE}
+sudo echo '  </Directory>' >> ${DEFAULT_SITE}
+sudo echo '</VirtualHost>' >> ${DEFAULT_SITE}
+
+sudo apachectl restart
 
 touch /tmp/provision_web
