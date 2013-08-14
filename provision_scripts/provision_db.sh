bf48a949a39614d65ac8726ef8805c77df8ac733
Bryan Everly <bryan.everly@teradata.com>
2013-08-14 17:25:50 -0400

    Cleaned up db provision script some more

--- provision_scripts/provision_db.sh
+++ provision_scripts/provision_db.sh
@@ -9,18 +9,16 @@ fi
 echo '192.168.56.100 web web.local' >> /etc/hosts
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get --quiet update
-sudo apt-get --quiet -y install postgresql-9.2
+sudo apt-get --quiet update > /dev/null
+sudo apt-get --quiet -y install postgresql-9.2 > /dev/null
 
 sudo -u postgres psql template1 << EOF
   ALTER USER postgres with encrypted password 'postgres';
 EOF
-#sudo passwd postgres
 
 sudo sh -c 'echo "host all all samenet md5" >> /etc/postgresql/9.2/main/pg_hba.conf'
 sed "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.2/main/postgresql.conf > /tmp/postgresql.conf
 sudo mv /tmp/postgresql.conf /etc/postgresql/9.2/main/postgresql.conf
-sudo rm /tmp/postgresql.conf
 sudo service postgresql restart
 
 touch /tmp/provision_db
