5c014dd8aef9856f7dd84f2bb006d87c45e8547d
Bryan Everly <bryan.everly@teradata.com>
2013-08-19 21:34:32 -0400

    Updated LegacyDLL

---
+++ LegacyDLL/LegacyDLL/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyDLL
+{
+    public class Product
+    {
+        private int _id;
+        private string _name;
+        private string _comments;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Comments
+        {
+            get
+            {
+                return _comments;
+            }
+            set
+            {
+                _comments = value;
+            }
+        }
+    }
+}
---
+++ LegacyDLL/LegacyDLL/ProductAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace LegacyDLL
+{
+    public class ProductAPI
+    {
+        public Product getProductById(int id)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection("Server=192.168.56.101;Port=5432;User Id=postgres;Password=postgres;Database=marketingops;");
+            conn.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand("select * from product where id=" + id.ToString(), conn);
+            NpgsqlDataReader dr = command.ExecuteReader();
+            if (dr.Read())
+            {
+                Product returnVal = new Product();
+                returnVal.Id = (int)dr["id"];
+                returnVal.Name = (string)dr["name"];
+                returnVal.Comments = (string)dr["comments"];
+
+                return returnVal;
+            }
+
+            return null;
+        }
+    }
+}
