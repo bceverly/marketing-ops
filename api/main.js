68edc24cb770c9f3b223ae52795daf7dd43711f4
Bryan Everly <bryan.everly@teradata.com>
2013-08-21 11:01:10 -0400

    Added thrift to api

--- api/main.js
+++ api/main.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var pg = require('pg');
 var url = require('url');
+var thrift = require('thrift');
+var ttransport = require('thrift/lib/thrift/transport');
+var service = require('../ThriftBridgeServer/gen-nodejs/ProductService');
+var types = require('../ThriftBridgeServer/gen-nodejs/ThriftBridgeServer_types');
 
 var app = express();
 var conString = "tcp://postgres:postgres@db.local/marketingops";
@@ -13,6 +17,53 @@ app.configure(function() {
 });
 
 console.log('Ready to process...');
+
+app.get('/api/product/:id', function(req, res) {
+	console.log('API call for product lookup by ID made.');
+	
+	var connection = thrift.createConnection('192.168.56.102', 9090, {transport: ttransport.TBufferedTransport});
+	var client = thrift.createClient(service, connection);
+	
+	connection.on('error', function(err) {
+	  console.error(err);
+      res.send(500, err);
+      res.end();
+	});
+
+	client.GetProductById(1, function(err, response) {
+	  if (err) {
+	    console.error(err);
+		connection.end();
+        res.send(500, err);
+		connection.end();
+        res.end();
+	  } else {
+		if (typeof response === 'undefined')
+		{
+            // Product was not found.  Return appropriate HTTP error code.
+            console.log('customer not found for id ' + req.params.id);
+            res.json(null, 404);
+            res.end();
+		} else if (null == response) {
+            // Product was not found.  Return appropriate HTTP error code.
+            console.log('customer not found for id ' + req.params.id);
+            res.json(null, 404);
+            res.end();
+		} else {
+			// Product found.  Return result.
+			var url_parts = url.parse(req.url, true);
+			var query = url_parts.query;
+
+			var response = JSON.stringify(result);
+			var response = JSON.stringify(result.rows[0]);
+			response = query.callback + '(' + response + ');';
+			console.log('response = ' + response);
+			connection.end();
+			res.end(response);
+		}
+	  }
+	});
+});
 
 app.get('/api/customer/:id', function(req,res) {
   console.log('API call for customer lookup by ID made.');
