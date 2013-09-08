58db66d03fbde4f15895307cf892f778f5c57776
Bryan Everly <bryan.everly@teradata.com>
2013-09-08 16:00:01 -0400

    Updated diagram yet again

--- provision_scripts/provision_api.sh
+++ provision_scripts/provision_api.sh
@@ -13,8 +13,8 @@ echo '192.168.56.102 win win.local' >> /etc/hosts
 echo '192.168.56.103 api api.local' >> /etc/hosts
 echo '192.168.56.104 custom custom.local' >> /etc/hosts
 
-sudo forever start -a /srv/api/mainRestServer.js
-sudo forever start -a /srv/custom/customizationRestServer.js
+sudo forever start -a /srv/api/mainRestServer.js --minUptime 1000 --spinSleepTime 10000
+sudo forever start -a /srv/custom/customizationRestServer.js --minUptime 1000 --spinSleepTime 10000
 
 sudo apt-get --quiet -y install redis-server > /dev/null
 
