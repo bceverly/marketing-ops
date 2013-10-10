7f18b1c86c64f794ba741299d5a04713dfd6b644
Bryan Everly <bryan.everly@teradata.com>
2013-10-10 10:41:38 -0400

    Updated for PostgreSQL 9.3

--- provision_scripts/provision_db.sh
+++ provision_scripts/provision_db.sh
@@ -15,16 +15,16 @@ echo '192.168.56.104 custom custom.local' >> /etc/hosts
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get --quiet update > /dev/null
-sudo apt-get --quiet -y install postgresql-9.2 > /dev/null
+sudo apt-get --quiet -y install postgresql-9.3 > /dev/null
 
 sudo -u postgres psql template1 << EOF
   ALTER USER postgres with encrypted password 'postgres';
   CREATE DATABASE marketingops OWNER postgres;
 EOF
 
-sudo sh -c 'echo "host all all samenet md5" >> /etc/postgresql/9.2/main/pg_hba.conf'
-sed "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.2/main/postgresql.conf > /tmp/postgresql.conf
-sudo mv /tmp/postgresql.conf /etc/postgresql/9.2/main/postgresql.conf
+sudo sh -c 'echo "host all all samenet md5" >> /etc/postgresql/9.3/main/pg_hba.conf'
+sed "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.3/main/postgresql.conf > /tmp/postgresql.conf
+sudo mv /tmp/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
 sudo service postgresql restart
 
 liquibase --driver=org.postgresql.Driver --changeLogFile=/srv/db/marketingops.xml --url="jdbc:postgresql://192.168.56.101/marketingops" --username=postgres --password=postgres --classpath=/usr/local/bin/postgresql-9.2-1003.jdbc4.jar update
